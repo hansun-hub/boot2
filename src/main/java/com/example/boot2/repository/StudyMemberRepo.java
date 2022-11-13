30666dd5ba4b61b4a399b2654eb59adc431b0514
hansun-hub <hansun33@naver.com>
2022-11-13 11:41:02 +0900

    2022-11-13_#13. 테이블 join

--- src/main/java/com/example/boot2/repository/StudyMemberRepo.java
+++ src/main/java/com/example/boot2/repository/StudyMemberRepo.java
@@ -8,4 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface StudyMemberRepo extends JpaRepository<Study_member, Integer> {
 
+    Study_member findByLoginId(String loginId);
+
 }
